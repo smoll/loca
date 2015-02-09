8897553a476037a5f94b1adbb279628bae6886b4
Shujon Mollah <smollah@theorchard.com>
2015-02-09 02:33:55 -0500

    Add tests for CLI#yes?

--- spec/loca/cli_spec.rb
+++ spec/loca/cli_spec.rb
@@ -13,7 +13,7 @@ describe Loca::CLI do
       expect_any_instance_of(Loca::Git).to receive(:branch_name).and_return 'PULL_1'
       expect_any_instance_of(Loca::Git).to receive(:first_time_creating?).and_return true
 
-      output = capture(:stdout) { Loca::CLI.start(%W(c #{url})) }.strip
+      output = capture(:stdout) { described_class.start(%W(c #{url})) }.strip
       expect(output).to end_with 'Checked out PULL_1!'
     end
 
@@ -22,7 +22,7 @@ describe Loca::CLI do
       expect_any_instance_of(Loca::Git).to receive(:first_time_creating?).and_return false
       silence(:stdout) { allow_any_instance_of(described_class).to receive(:yes?).and_return true }
 
-      output = capture(:stdout) { Loca::CLI.start(%W(c #{url})) }.strip
+      output = capture(:stdout) { described_class.start(%W(c #{url})) }.strip
       expect(output).to end_with 'Checked out PULL_1!'
     end
 
@@ -31,18 +31,18 @@ describe Loca::CLI do
       expect_any_instance_of(Loca::Git).to receive(:first_time_creating?).and_return false
       silence(:stdout) { allow_any_instance_of(described_class).to receive(:yes?).and_return false }
 
-      expect { silence(:stderr) { Loca::CLI.start(%W(c #{url})) } }.to raise_error
+      expect { silence(:stderr) { described_class.start(%W(c #{url})) } }.to raise_error
     end
 
     it 'raises an error when an invalid repo is supplied' do
-      expect { silence(:stderr) { Loca::CLI.start(%W(c #{invalid_url})) } }.to raise_error
+      expect { silence(:stderr) { described_class.start(%W(c #{invalid_url})) } }.to raise_error
     end
 
     it 'deletes when the delete flag is appended' do
       expect_any_instance_of(Loca::Git).to receive(:branch_name).and_return 'PULL_1'
       expect_any_instance_of(Loca::Git).to receive(:delete)
 
-      output = capture(:stdout) { Loca::CLI.start(%W(c #{url} -d)) }.strip
+      output = capture(:stdout) { described_class.start(%W(c #{url} -d)) }.strip
       expect(output).to eq 'Deleted PULL_1!'
     end
   end
@@ -52,18 +52,32 @@ describe Loca::CLI do
       expect_any_instance_of(Loca::Git).to receive(:branch_name).and_return 'PULL_1'
       expect_any_instance_of(Loca::Git).to receive(:delete)
 
-      output = capture(:stdout) { Loca::CLI.start(%W(d #{url})) }.strip
+      output = capture(:stdout) { described_class.start(%W(d #{url})) }.strip
       expect(output).to eq 'Deleted PULL_1!'
     end
 
     it 'raises an error when an invalid repo is supplied' do
-      expect { silence(:stderr) { Loca::CLI.start(%W(d #{invalid_url})) } }.to raise_error
+      expect { silence(:stderr) { described_class.start(%W(d #{invalid_url})) } }.to raise_error
     end
   end
 
   describe '--version' do
     it 'returns the correct version' do
-      expect(capture(:stdout) { Loca::CLI.start(%w(-v)) }.strip).to eq(Loca::VERSION)
+      expect(capture(:stdout) { described_class.start(%w(-v)) }.strip).to eq(Loca::VERSION)
+    end
+  end
+
+  describe '#yes?' do
+    it "returns true when user types 'yes'" do
+      allow(subject.instance_variable_get(:@shell)).to receive(:gets).and_return 'yes'
+
+      expect(subject.send(:yes?, 'Is this the real life?')).to eq true
+    end
+
+    it "returns false when user types 'no'" do
+      allow(subject.instance_variable_get(:@shell)).to receive(:gets).and_return 'no'
+
+      expect(subject.send(:yes?, 'Is this just fantasy?')).to eq false
     end
   end
 end
